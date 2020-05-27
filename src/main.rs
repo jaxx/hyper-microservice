e8befe34ef0992f3d8cc1c5144d5ac5c54175cc9
Jakko Sikkar <jakko.sikkar@gmail.com>
2020-05-27 22:23:41 +0300

    handle paths with regular expressions

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use std::task::{Context, Poll};
 use futures::future;
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use hyper::service::Service;
+use lazy_static::lazy_static;
+use regex::Regex;
 use slab::Slab;
 
 type UserId = u64;
@@ -23,7 +25,11 @@ const INDEX: &str = r#"
 </html>
 "#;
 
-const USER_PATH: &str = "/user/";
+lazy_static! {
+    static ref INDEX_PATH: Regex = Regex::new("^/(index\\.html?)?$").unwrap();
+    static ref USER_PATH: Regex = Regex::new("^/user/((?P<user_id>\\d+?)/?)?$").unwrap();
+    static ref USERS_PATH: Regex = Regex::new("^/users/?$").unwrap();
+}
 
 #[derive(Debug)]
 struct UserData;
@@ -70,57 +76,72 @@ impl Service<Request<Body>> for MicroService {
 
     fn call(&mut self, req: Request<Body>) -> Self::Future {
         let response = {
-            match (req.method(), req.uri().path()) {
-                (&Method::GET, "/") => {
+            let method = req.method();
+            let path = req.uri().path();
+            let mut users = self.user_db.lock().unwrap();
+
+            if INDEX_PATH.is_match(path) {
+                if method == &Method::GET {
                     Response::new(INDEX.into())
-                },
-                (method, path) if path.starts_with(USER_PATH) => {
-                    let user_id = path.trim_start_matches(USER_PATH)
-                                                     .parse::<UserId>()
-                                                     .ok()
-                                                     .map(|x| x as usize);
-
-                    let mut users = self.user_db.lock().unwrap();
-
-                    match (method, user_id) {
-                        (&Method::POST, None) => {
-                            let id = users.insert(UserData);
-                            Response::new(id.to_string().into())
-                        },
-                        (&Method::POST, Some(_)) => {
-                            response_with_code(StatusCode::BAD_REQUEST)
-                        },
-                        (&Method::GET, Some(id)) => {
-                            if let Some(data) = users.get(id) {
-                                Response::new(data.to_string().into())
-                            } else {
-                                response_with_code(StatusCode::NOT_FOUND)
-                            }
-                        },
-                        (&Method::PUT, Some(id)) => {
-                            if let Some(data) = users.get_mut(id){
-                                *data = UserData;
-                                response_with_code(StatusCode::OK)
-                            } else {
-                                response_with_code(StatusCode::NOT_FOUND)
-                            }
-                        },
-                        (&Method::DELETE, Some(id)) => {
-                            if users.contains(id) {
-                                users.remove(id);
-                                response_with_code(StatusCode::OK)
-                            } else {
-                                response_with_code(StatusCode::NOT_FOUND)
-                            }
-                        },
-                        _ => {
-                            response_with_code(StatusCode::METHOD_NOT_ALLOWED)
-                        }
-                    }
-                },
-                _ => {
-                    response_with_code(StatusCode::NOT_FOUND)
+                } else {
+                    response_with_code(StatusCode::METHOD_NOT_ALLOWED)
                 }
+            } else if USERS_PATH.is_match(path) {
+                if method == &Method::GET {
+                    let list = users.iter()
+                                            .map(|(id, _)| id.to_string())
+                                            .collect::<Vec<String>>()
+                                            .join(",");
+
+                    Response::new(list.into())
+                } else {
+                    response_with_code(StatusCode::METHOD_NOT_ALLOWED)
+                }
+            } else if let Some(cap) = USER_PATH.captures(path) {
+                let user_id = cap.name("user_id").and_then(|m| {
+                    m.as_str()
+                        .parse::<UserId>()
+                        .ok()
+                        .map(|x| x as usize)
+                });
+
+                match (method, user_id) {
+                    (&Method::POST, None) => {
+                        let id = users.insert(UserData);
+                        Response::new(id.to_string().into())
+                    },
+                    (&Method::POST, Some(_)) => {
+                        response_with_code(StatusCode::BAD_REQUEST)
+                    },
+                    (&Method::GET, Some(id)) => {
+                        if let Some(data) = users.get(id) {
+                            Response::new(data.to_string().into())
+                        } else {
+                            response_with_code(StatusCode::NOT_FOUND)
+                        }
+                    },
+                    (&Method::PUT, Some(id)) => {
+                        if let Some(data) = users.get_mut(id){
+                            *data = UserData;
+                            response_with_code(StatusCode::OK)
+                        } else {
+                            response_with_code(StatusCode::NOT_FOUND)
+                        }
+                    },
+                    (&Method::DELETE, Some(id)) => {
+                        if users.contains(id) {
+                            users.remove(id);
+                            response_with_code(StatusCode::OK)
+                        } else {
+                            response_with_code(StatusCode::NOT_FOUND)
+                        }
+                    },
+                    _ => {
+                        response_with_code(StatusCode::METHOD_NOT_ALLOWED)
+                    }
+                }
+            } else {
+                response_with_code(StatusCode::NOT_FOUND)
             }
         };
 
